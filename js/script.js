7aa9f72ba8a418a11b6b4dfde4a1a403f73255bf
Phyllis Feng <phyllisfeng@Phylliss-MacBook-Pro.local>
2022-07-31 19:51:23 -0400

    Personal website

--- js/script.js
+++ js/script.js
@@ -60,7 +60,7 @@ $(document).ready(function() {
     $(window).scroll( function(){
         $('#Work h1, #tldr, #housebayou, #pamelas, #falooda, #aboutHeadings,\
         #selfie, #brunch, #incline, #mural, #cupcake, #apples, #reflection, #cat,\
-        #bio, #siteInfo, #footer').each( function(i){
+        #catCaption, #bio, #siteInfo, #footer').each( function(i){
             
             var bottom_of_object = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
